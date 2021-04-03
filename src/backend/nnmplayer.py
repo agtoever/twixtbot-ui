df12f4f9fd0bd55d978d829a9f757606ee8540da
Albert ten Oever <agtoever@hotmail.com>
2021-04-03 21:12:01 +0200

    Merged master into code for adjusting bot level

--- src/backend/nnmplayer.py
+++ src/backend/nnmplayer.py
@@ -1,6 +1,24 @@
-#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Neural Network/Monte Carlo Twixt Bot Player
+
+This module implements a Twixt playing bot based on Jordan Lampe's
+twixtbot (https://github.com/BonyJordan/twixtbot), which is based on
+AlphaZero (https://en.wikipedia.org/wiki/AlphaZero) neural network /
+Monte Carlo simulation techniques.
+
+The module was adjusted to fit into the twixtbot-gui. Alterations include:
+    - creating callbacks for threaded Monte Carlo evaluation, so that
+      the progress of the evaluation can be shown in the GUI.
+    - replacing the pick_move method with and eval_game method, which
+      allows the user to adjust the bot's strength.
+
+"""
+
+# Module imports of standard Python libraries
 import numpy
 import random
+
+# Module imports of Joran Lampe's twixtbot libraries
 import logging
 import constants as ct
 import backend.naf as naf
@@ -10,12 +28,80 @@ import backend.swapmodel as swapmodel
 import backend.twixt as twixt
 
 
+def _select_move(moves, P, bot_strength):
+    """ Returns a move based on moves, P and bot_strength
+
+    Note:
+        A bot_strength < 1.0 results in non-deterministic behaviour
+
+    Args:
+        moves (List(twixt.Point)): sorted list of moves; best first
+        P (List(float)): P-score for each move [0,1] (higher=better)
+        bot_strength ([float]): bot strengt: 0 = weak; 1 = best
+
+    Returns:
+        twixt.Point: selected move
+    """
+
+    # Normalize bot strength
+    w = min(0.99, max(bot_strength, 0.0001))
+
+    # Normalize P
+    Parr = numpy.array(P)
+    Pnorm = (Parr/sum(Parr))**(w/(1-w))
+
+    # Return P-weighted choice from moves
+    sel_move = random.choices(moves, weights=Pnorm)[0]
+    print(f'selected move: {sel_move} ({moves.index(sel_move)}-best choice)')
+    print(f'Top 10 moves + P: {dict(zip(moves[:10], P[:10]))}')
+    return sel_move
+
+
+def _P1k(P):
+    """Scale P list from [0.0, 1.0] to [0, 1000]"""
+    return [int(round(1000 * p)) for p in P]
+
+
 class Player:
+    """Implements the bot player.
+
+    Attributes (type, default values between brackets):
+
+        *** attributes related to the AlphaZero model ***
+        model (str, None): path to saved tenslorflow model ('saved_model.pb')
+        num_trials (int, 100): number of Monte Carlo simulations to run.
+        temperature (int, 0): controls the policy which move is taken after
+            the MCTS. Valid values are one of: 0.0, 0.5 or 1.0
+        random_rotation (int, 0): random rotate/mirror the board (0 or 1)
+        smart_root (int, 0): smart-select the MCTS starting moves (0 or 1)
+        allow_swap (int, 1): allow swapping in second move (0 or 1)
+        add_noise (int, 0): add dirichlet noise to P (0 or 1)
+        cpuct (int, 1): MCTS constant that balances exploitation (higher
+            value) versus exploration (lower value).
+        verbosity (int, 0): prints more status info (0, 1, 2 or 3)
+
+        *** other attributes ***
+        cached_game_eval (dict): cached versions of (score, P, moves) tuple
+            with hash(game) as dict key.
+        nm (nnmcts.NeuralMCTS): the actual Neural MCTS model
+
+        # TODO: implement send_message in this class and make nm.eval_game
+        # respond to the GUI using a callback. This untangles nnmplayer
+        # and nnmcts a bit more...
+    """
 
     def __init__(self, **kwargs):
+        """ Creates the bot player.
+
+        Args:
+            **kwargs (dict): bot parameters (see class attributes)
+
+        Raises:
+            ValueError if an invalid bot parameter is given.
+        """
         self.logger = logging.getLogger(ct.LOGGER)
         self.model = kwargs.get('model', None)
-        self.num_trials = int(kwargs.get('trials', 100))
+        self.trials = int(kwargs.get('trials', 100))
         self.temperature = float(kwargs.get('temperature', 0))
         self.random_rotation = int(kwargs.get('random_rotation', 0))
 
@@ -37,7 +123,6 @@ class Player:
             nneval_ = self.evaluator
 
             def nnfunc(game):
-
                 nips = naf.NetInputs(game)
                 if self.random_rotation:
                     rot = random.randint(0, 3)
@@ -54,7 +139,7 @@ class Player:
                     ml = ml[0]
                 return pw, ml
         else:
-            raise Exception("Specify model or resource")
+            raise ValueError("Specify model or resource")
 
         self.nm = nnmcts.NeuralMCTS(
             nnfunc,
@@ -65,20 +150,67 @@ class Player:
             visualize_mcts=False
         )
 
-    def pick_move(self, game, window=None, event=None):
-        if self.allow_swap and len(game.history) < 2:
+    def pick_move(self,
+                  game: twixt.Game,
+                  window=None,
+                  event=None):
+        raise NotImplementedError
 
+    def eval_game(self, game: twixt.Game,
+                  bot_strength=1.0, use_mcts=False,
+                  window=None, event=None):
+        """ Let the robot evaluate the game and return game statistics.
+
+        Notes:
+            The underlying nm.eval_game are cached by this function, so there
+            is no performance penalty if eval_game is called multiple times
+            for the same game state.
+
+            A bot_strength < 1.0 results in non-deterministic behaviour
+
+        Args:
+            game (twixt.Game): Twixt game for which the bot evaluates.
+            bot_strength (float): bot strengt: 0 = weak; 1 = best. Default 1.
+            use_mcts (Bool): use Monte Carlo Simulator. Default False.
+            window (PySimpleGUI.Window, optional): windows used for sending
+                status updates. Defaults to None.
+            event (threading.Event): thread stops if get_state() is True
+                MCTS thread status. Defaults to None.
+
+        Returns:
+            (score, moves, P, botmove) (Tuple):
+                score (float): value head [-1,1] for [black,white]'s strength
+                moves (List(twixt.Point)): sorted list of moves; best first
+                P (List(float)): P-score for each move [0,1] (higher=better)
+                botmove (twixt.Point): move chosen based on bot_strength
+        """
+
+        # Evaluate game; uses cached data if possible.
+        score, moves, P = self.nm.eval_game(game)
+
+        # Handle first en second move special cases
+        if self.allow_swap and len(game.history) < 2:
             if len(game.history) == 0:
                 self.report = "swapmodel"
-                m = swapmodel.choose_first_move()
-                return self.nm.create_response(game, "done", moves=[m])
+                m = [swapmodel.choose_first_move()] + moves
+                P = [1000] + P
+                selected_move = _select_move(m, P, bot_strength)
+                self.nm.send_message(window, game, "done", moves=m, P=P,
+                                     selected_move=selected_move)
+                return (score, m, P, selected_move)
+
             elif swapmodel.want_swap(game.history[0]):
                 self.report = "swapmodel"
-                m = twixt.SWAP
-                return self.nm.create_response(game, "done", moves=[m])
+                m = [twixt.SWAP] + moves
+                P = [1000] + P
+                selected_move = _select_move(m, P, bot_strength)
+                self.nm.send_message(window, game, "done", moves=m, P=P,
+                                     selected_move=selected_move)
+                return (score, m, P, selected_move)
             # else:
             #   didn't want to swap => compute move
 
+<<<<<<< HEAD
         if self.num_trials == 0:
             # don't use MCTS but just evaluate and return best move
             _, moves, P = self.nm.eval_game(game)
@@ -87,16 +219,34 @@ class Player:
                                            P=[int(round(p * 1000)) for p in P])
 
         N = self.nm.mcts(game, self.num_trials, window, event)
+=======
+        if self.trials == 0 or not use_mcts:
+            # don't use MCTS
+            selected_move = _select_move(moves, P, bot_strength)
+            self.nm.send_message(window, game, "done", 0, 0, moves=moves,
+                                 P=P, selected_move=selected_move)
+            return (score, moves, P, selected_move)
+>>>>>>> be2d40f (Merged master into code for adjusting bot level)
 
+        N = self.nm.mcts(game, self.trials, window, event)
         self.report = self.nm.report
 
         # When a forcing win or forcing draw move is found, there's no policy
         # array returned
         if isinstance(N, (str, twixt.Point)):
+<<<<<<< HEAD
             return self.nm.create_response(game, "done", self.num_trials,
                                            self.num_trials, True,
                                            P=[1000, 0, 0])
+=======
+            P = [1000, 0, 0]
+            self.nm.send_message(window, game, "done", self.trials,
+                                 self.trials, True, P=[1000], moves=[N],
+                                 selected_move=N)
+            return (score, [N], [1000], N)
+>>>>>>> be2d40f (Merged master into code for adjusting bot level)
 
+        """
         if self.temperature == 0.0:
             mx = N.max()
             weights = numpy.where(N == mx, 1.0, 0.0)
@@ -104,6 +254,7 @@ class Player:
             weights = N
         elif self.temperature == 0.5:
             weights = N ** 2
+<<<<<<< HEAD
         self.logger.debug("weights=%s", weights)
 
         # flake8 said index isn't used. Not sure why this code is here.
@@ -113,3 +264,18 @@ class Player:
 
         return self.nm.create_response(game, "done", self.num_trials,
                                        self.num_trials, False)
+=======
+        self.logger.info("weights=%s", weights)
+        index = numpy.random.choice(numpy.arange(
+            len(weights)), p=weights / weights.sum())
+
+        self.nm.send_message(window, game, "done", self.trials,
+                             self.trials, False)
+        """
+
+        moves = [naf.policy_index_point(game, idx) for idx in range(len(N))]
+        selected_move = _select_move(moves, N, bot_strength)
+        self.nm.send_message(window, game, "done", 0, 0, moves=moves,
+                             P=_P1k(P), selected_move=selected_move)
+        return (score, moves, _P1k(P), selected_move)
+>>>>>>> be2d40f (Merged master into code for adjusting bot level)
--- src/tbui.py
+++ src/tbui.py
@@ -202,14 +202,13 @@ class TwixtbotUI():
         self.get_control(ct.K_MOVES).Update(text)
 
     def calc_eval(self):
-        score, moves, P = self.bots[self.game.turn].nm.eval_game(self.game)
+        self.next_move = self.bots[self.game.turn].nm.eval_game(self.game, self.game.SIZE)
         # get score from white's perspective
-        sc = round((2 * self.game.turn - 1) * score, 3)
-        self.next_move = sc, moves, P
+        sc = round((2 * self.game.turn - 1) * self.next_move[0], 3)
         # Add sc to dict of historical scores
         self.moves_score[len(self.game.history)] = sc
 
-        return sc, moves, P
+        return self.next_move
 
     def clear_evals(self):
         self.get_control(ct.K_EVAL_NUM).Update('')
@@ -373,6 +372,7 @@ class TwixtbotUI():
         import backend.nnmplayer as nnmplayer
         self.bots[2 - player] = nnmplayer.Player(**args)
 
+<<<<<<< HEAD
     def call_bot(self):
         # mcts, or first/second move (we are in a thread)
         response = self.bots[self.game.turn].pick_move(
@@ -383,6 +383,60 @@ class TwixtbotUI():
             # so execute move.
             # execute move must be inside thread!
             self.execute_move(response["moves"][0])
+=======
+    def call_bot_and_move(self):
+        # TODO!
+        """
+>>>>>>> b1c0e6e (Committing temporary state of work)
+        if len(self.game.history) >= 2 and self.get_current(ct.K_TRIALS) == 0 and self.next_move is not None:
+            # we already have the next move from evaluation            
+            response = {
+                "status": "done",
+                "moves": self.next_move[1],
+                "P": self.next_move[2]
+            }
+            
+        else:
+            # mcts, or first/second move       
+            response = self.bots[self.game.turn].pick_move(self.game, self.window, self.bot_event)
+        """
+        logging.debug('Calling pick_move (this needs to be improved)!')
+        # mcts, or first/second move
+        self.bots[self.game.turn].pick_move(
+            self.game, self.window, self.event, 0.0)
+
+        if response["status"] == "done":
+            self.get_control(ct.K_SPINNER).Update(visible=False)
+            if self.bot_event is None or not self.bot_event.is_set() or self.bot_event.get_context() == ct.ACCEPT_EVENT:
+                # bot has not been cancelled (but is finished or accepted)
+                self.execute_move(response["moves"][0])
+                self.update_after_move(False)
+            else:
+                # bot has been cancelled clear progress controls and visits
+                self.update_progress()
+                # reset history_at_root resets tree and visit counts
+                self.bots[self.game.turn].nm.history_at_root = None
+
+                # switch off auto move
+                if self.get_current(ct.K_AUTO_MOVE):
+                    self.set_current(ct.K_AUTO_MOVE, False)
+                    self.get_control(
+                        ct.K_AUTO_MOVE, self.game.turn_to_player()).Update(False)
+
+    def launch_bot(self):
+        
+        if self.get_current(ct.K_TRIALS) > 0:
+            self.visit_plot.update()
+            self.update_progress()            
+            self.window[ct.K_SPINNER[1]].Update(visible=True)
+            # MCTS => async
+            self.bot_event = BotEvent()
+            self.thread = threading.Thread(
+                target=self.call_bot_and_move, args=(), daemon=True)
+    
+            self.timer = pmeter.ETA(100.0, max_seconds=20)
+            self.thread.start()
+>>>>>>> be2d40f (Merged master into code for adjusting bot level)
         else:
             # reset history_at_root resets tree and visit counts
             self.bots[self.game.turn].nm.history_at_root = None
@@ -501,6 +555,27 @@ class TwixtbotUI():
                 len(values["moves"]) > 1):
             self.visit_plot.update(values, max(1, values["max"]))
 
+<<<<<<< HEAD
+=======
+        """ Code adjusted for adjustable bot level, but not needed anymore???
+        if values["status"] == "done":
+            self.get_control(ct.K_SPINNER).Update(visible=False)
+            if not self.event.is_set() or self.event.get_context() == ct.ACCEPT_EVENT:
+                # bot has not been cancelled (but is finished or accepted)
+                print(f'Values: {values}')
+                self.execute_move(values["moves"][0])
+                self.update_after_move()
+            else:
+                # clear progress controls
+                self.update_progress()
+                # switch off auto move
+                if self.get_current(ct.K_AUTO_MOVE):
+                    self.set_current(ct.K_AUTO_MOVE, False)
+                    self.get_control(
+                        ct.K_AUTO_MOVE, self.game.turn_to_player()).Update(False)
+        """
+
+>>>>>>> be2d40f (Merged master into code for adjusting bot level)
     def handle_accept_and_cancel(self, event):
         if event == ct.B_ACCEPT:
             self.handle_accept_bot()
